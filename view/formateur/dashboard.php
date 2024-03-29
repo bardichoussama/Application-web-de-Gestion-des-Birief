c2ccc8e41d2eb8a081d6db51698239dd43b81015
oussama bardich <amv.0280@gmail.com>
2024-03-29 02:36:46 +0000

    modified:   view/formateur/dashboard.php

--- view/formateur/dashboard.php
+++ view/formateur/dashboard.php
@@ -55,7 +55,7 @@
                     <path fill-rule="evenodd" d="M9 16a6 6 0 1 1 12 0 6 6 0 0 1-12 0Zm6-3a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 1 1 0-2h1v-1a1 1 0 0 1 1-1Z" clip-rule="evenodd" />
                   </svg>
 
-                  <span class="ml-3 text-xl">Create Brief</span>
+                  <span class="ml-3 text-xl text-gray-900  dark:text-gray-200">Create Brief</span>
                 </a>
               </li>
               <ul class="space-y-8">
@@ -65,7 +65,7 @@
                       <path fill-rule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clip-rule="evenodd" />
                     </svg>
 
-                    <span class="ml-3 text-xl">Learners</span>
+                    <span class="ml-3 text-xl text-gray-900  dark:text-gray-200">Learners</span>
                   </a>
                 </li>
                 <li>
@@ -74,7 +74,7 @@
                       <path fill-rule="evenodd" d="M5.024 3.783A1 1 0 0 1 6 3h12a1 1 0 0 1 .976.783L20.802 12h-4.244a1.99 1.99 0 0 0-1.824 1.205 2.978 2.978 0 0 1-5.468 0A1.991 1.991 0 0 0 7.442 12H3.198l1.826-8.217ZM3 14v5a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5h-4.43a4.978 4.978 0 0 1-9.14 0H3Z" clip-rule="evenodd" />
                     </svg>
 
-                    <span class="ml-3 text-xl">Inbox</span>
+                    <span class="ml-3 text-xl text-gray-900  dark:text-gray-200">Inbox</span>
                   </a>
                 </li>
                 <li>
@@ -84,7 +84,7 @@
                       <path fill-rule="evenodd" d="M4 7a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2H4Zm0 11v-5.5h11V18H4Z" clip-rule="evenodd" />
                     </svg>
 
-                    <span class="ml-3 text-xl">Pages</span>
+<span class="ml-3 text-xl text-gray-900 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-300">Pages</span>
                   </a>
                 </li>
                 <li>
@@ -94,17 +94,17 @@
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                     </svg>
 
-                    <span class="ml-3 text-xl">Settings</span>
+                    <span class="ml-3 text-xl text-gray-900  dark:text-gray-200 ">Settings</span>
                   </a>
                 </li>
-                <ul class="pt-5 mt-5 space-y-8 border-t border-gray-200 ">
+                <ul class="pt-5 mt-5 space-y-8 border-t border-gray-200 dark:border-gray-700">
                   <li>
                     <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 ">
                       <svg style="color: #3AAFA9;" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
                       </svg>
 
-                      <span class="ml-3 text-xl">Logout</span>
+                      <span class="ml-3 text-xl text-gray-900  dark:text-gray-200">Logout</span>
                     </a>
                   </li>
 
@@ -112,7 +112,7 @@
       </aside>
     </div>
     <div class="flex justify-center">
-      <div class=" bg-white mt-7 w-96 rounded-xl " style="width: 1480px; height: 1030px; ">
+      <div class="  mt-7 w-96 rounded-xl text-gray-900 bg-white dark:bg-gray-800 dark:text-gray-200 " style="width: 1480px; height: 1030px;  ">
         <h3 class="text-5xl font-semibold ml-7 mt-7">Hello,<span class="text-regular text-4xl ml-4">Imane</span></h3>
         <button id="theme-toggle" type="button" class="text-gray-500  hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
           <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -123,7 +123,7 @@
           </svg>
         </button>
 
-        <div class="flex bg-white border rounded-xl mx-4 h-24 p-4 mt-16">
+        <div class="flex  dark:bg-gray-900 border rounded-xl mx-4 h-24 p-4 mt-16 dark:border-gray-700">
           <img src="../../public/assets/img/bp-cover.jpg" alt="" class="w-24 h-16 rounded-xl ">
           <div class="ml-4  w-4/12">
             <p class="text-base font-medium">Creer une site web worddpress</p>
@@ -177,7 +177,7 @@
 
           <div id="fullWidthTabContent" class="border-t border-gray-200 ">
 
-            <div class="flex bg-white  rounded-xl h-24 mt-16">
+            <div class="flex bg-white  rounded-xl h-24 mt-16 ">
               <img src="../../public/assets/img/bp-cover.jpg" alt="" class="w-24 h-16 rounded-xl">
               <div class="ml-4  w-4/12">
                 <p class="text-base font-medium">Creer une site web worddpress</p>
