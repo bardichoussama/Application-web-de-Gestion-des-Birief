029a74401b8c1a1fa172ee1b71bcdb8ed0cacc4b
AyoubyAmazu <ayoubamazu@gmail.com>
2024-04-03 01:14:43 +0000

    new edit

---
+++ controller/apprenant/apprenantLogin.php
@@ -0,0 +1,17 @@
+<?php
+    require_once '../../config/db.php'; 
+    require_once '../../model/AuthenticationModel.php'; 
+    $db = $database->getConnection();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $authModel = new AuthenticationModel($db);
+        $loggedIn = $authModel->login($email, $password, 'APPRENANT'); 
+        if ($loggedIn) {
+                header('Location: ../../view/apprenant/dashboard.php'); 
+                exit();
+        } else {       
+            echo "Invalid email or password. Please try again.";
+        }
+    }
+?>
--- view/apprenant/apprenat_login.php
+++ view/apprenant/apprenat_login.php
@@ -29,7 +29,7 @@
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                         Apprenant Login
                     </h1>
-                    <form class="space-y-4 md:space-y-6 bg-white rounded-xl" method="post" action="../../controller/formateurLogin.php">
+                    <form class="space-y-4 md:space-y-6 bg-white rounded-xl" method="post" action="../../controller/apprenant/apprenantLogin.php">
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
                             <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
