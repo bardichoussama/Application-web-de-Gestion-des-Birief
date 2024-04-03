69c2c2202285e629d0b8b42035146d7a4b137f6c
AyoubyAmazu <ayoubamazu@gmail.com>
2024-04-03 13:20:12 +0000

    erreur dashboard done

--- controller/formateur/formateurDashboard.php
+++ controller/formateur/formateurDashboard.php
@@ -1,17 +1,20 @@
 <?php
     session_start();
-     require_once("../../config/db.php");
-     require_once("../../model/formateur/formateurInfo.php");
-     require_once("../../model/formateur/formateur_BP_model.php");
-        $conn=$database->getConnection();
-        $formateur = new formateur($_SESSION["ID"],$conn);
-        $cardInfo = $formateur->getFormation();
-        $brief = new brief($conn);
-        $currentBP = $brief->getCurrentBP($_SESSION["ID"]);
-        $formateurBP = $brief->getFormateurBP($_SESSION["ID"]);
-        for($i=0;$i<count($formateurBP);$i++){
-        }
-        
+    if( $_SESSION["ID"]!="" && is_numeric($_SESSION["ID"]) ){
+        require_once("../../config/db.php");
+        require_once("../../model/formateur/formateurInfo.php");
+        require_once("../../model/formateur/formateur_BP_model.php");
+            $conn=$database->getConnection();
+            $formateur = new formateur($_SESSION["ID"],$conn);
+            $cardInfo = $formateur->getFormation();
+            $brief = new brief($conn);
+            $currentBP = $brief->getCurrentBP($_SESSION["ID"]);
+            $formateurBP = $brief->getFormateurBP($_SESSION["ID"]);
+            for($i=0;$i<count($formateurBP);$i++){
+            }
+    }else{
+        header("location:../../view/formateur/formateurLogin.php");
+    }
         
 
 ?>
--- view/formateur/dashboard_formateur.php
+++ view/formateur/dashboard_formateur.php
@@ -103,7 +103,7 @@ require_once "../../controller/formateur/formateurDashboard.php";
                 </li>
                 <ul class="pt-5 mt-5 space-y-8 border-t border-gray-200 dark:border-gray-700">
                   <li>
-                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 ">
+                    <a href="../../controller/formateur/logout.php" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 ">
                       <svg style="color: #3AAFA9;" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
                       </svg>
@@ -126,7 +126,7 @@ require_once "../../controller/formateur/formateurDashboard.php";
             <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
           </svg>
         </button>
-
+      <?php  if($currentBP):?>
         <div class="flex  dark:bg-gray-900 border rounded-xl mx-4 h-24 p-4 mt-16 dark:border-gray-700">
           <img src="../../public/assets/img/bp-cover.jpg" alt="" class="w-24 h-16 rounded-xl ">
           <div class="ml-4  w-4/12">
@@ -156,6 +156,7 @@ require_once "../../controller/formateur/formateurDashboard.php";
             </span>
           </div>
         </div>
+        
         <h4 class="text-xl font-medium mt-9 ml-4">Group progress</h4>
         <div class="bg-secondary w-6/12 h-20 rounded-xl mt-9 ml-8 px-4 flex justify-center items-center gap-11">
           <div class="w-16 h-12 bg-primary flex justify-center items-center rounded-lg">
@@ -171,7 +172,7 @@ require_once "../../controller/formateur/formateurDashboard.php";
             <span class="text-xl font-medium text-gray-800 ">8/13</span>
           </div>
         </div>
-       
+        <?php endif;?>
         <div class=" bg-white border border-gray-200 rounded-lg shadow-sm  sm:p-2 mt-11 mx-4">
           <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist" style="list-style-type: none; margin: 0; padding: 0;">
             <li class="w-full">
